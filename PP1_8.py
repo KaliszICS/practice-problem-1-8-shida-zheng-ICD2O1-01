601efa6e9f83299f805f3bf2ebbf0ec96a0b6327
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-09-27 15:22:54 +0000

    a

--- PP1_8.py
+++ PP1_8.py
@@ -1,19 +1,33 @@
 
 def q1():
   #Write Assignment code here
-
+  bool1 = True
+  bool2 = False
+  print(bool1 and bool2)
+  print(bool1 or bool2)
 def q2():
   #Write Assignment code here
-
+  num = int(input("Enter an integer: "))
+  bool1 = num != 0
+  print(bool1)
 def q3():
   #Write Assignment code here
-
+  num = int(input("Enter a number:"))
+  bool1 = 0 <= num <= 10
+  print(bool1)
 def q4():
   #Write Assignment code here
-
+  a = input("Input food:")
+  bool1 = a != "pizza"
+  b = input("Input drink:")
+  bool2 = b != "pop"
+  print(bool1)
+  print(bool2)
 def q5():
   #Write Assignment code here
-
+  num = int(input("Enter an integer:"))
+  bool1 = num % 2 == 0
+  print(bool1)
 #Do not edit code after this
 #Comment out the followwing code when running tests
 
