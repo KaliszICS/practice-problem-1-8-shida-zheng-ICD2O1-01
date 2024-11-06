371bc12dfad7da0754242c65a6ab6b679e630a95
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-06 12:15:54 -0500

    Update PP1_8.py

--- PP1_8.py
+++ PP1_8.py
@@ -1,13 +1,3 @@
-
-def q1():
-  #Write Assignment code here
-  bool1 = "True"
-  bool2 = "False"
-  print(bool1 and bool2)
-  print(bool1 or bool2)
-def q2():
-  #Write Assignment code here
-  num = int(input("Enter an integer: "))
 def q1():
   #Write Assignment code here
   bool1 = "True"
@@ -51,4 +41,5 @@ def q5():
 #q3()
 #q4()
 #q5()
+
 
